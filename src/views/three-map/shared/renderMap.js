378d4614fc4830ffb512f10013d9c3060e5d4400
chenshuideshizi <fanwenxiang326@163.com>
2022-01-26 22:34:37 +0800

    earcu调试成功

--- src/views/three-map/shared/renderMap.js
+++ src/views/three-map/shared/renderMap.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { Earcut } from 'three/src/extras/Earcut.js'
+import Earcut from 'earcut'
 
 const strategies = {
     BoxGeometry: {
@@ -46,45 +46,30 @@ export default function mapRender(objectDataList) {
 
 function createIrregularGeometry(points, height) {
     debugger
-    let topPoints = []
-    for (let i = 0; i < points.length; i++) {
-        let vertice = points[i]
-        topPoints.push([vertice[0], vertice[1] + height, vertice[2]])
-    }
+    let topPoints = points.map(point => [point[0], point[1] + height, point[2]])
 
     let length = points.length
     const faceVertices = []
     for (let j = 0; j < length; j++) { // 侧面生成三角形
-        let next
-        if (j != length - 1) {
-            // faceVertices.push(j, j + 1, length + j + 1)
-            // faceVertices.push(length + j + 1, length + j, j)
-            next = j + 1
-            faceVertices.push(topPoints[j], topPoints[next], points[j])
-            faceVertices.push(points[j], points[next], topPoints[next])
-        } else {
-            // faceVertices.push(j, 0, length)
-            // faceVertices.push(length, length + j, j)
-            next = 0
-            faceVertices.push(topPoints[j], topPoints[next], points[j])
-            faceVertices.push(points[j], points[next], topPoints[next])
-        }
+        const next = j === length - 1 ? 0 : j + 1
+        faceVertices.push(topPoints[j], topPoints[next], points[j])
+        faceVertices.push(points[j], points[next], topPoints[next])
     }
-    let data = []
-    for (let i = 0; i < length; i++) {
-        data.push(points[i][0], points[i][2])
-    }
-    let triangles = Earcut.triangulate(data)
+    let data = points.map(point => [point[0], point[2]]).flat()
+
+    let triangles = Earcut(data, [], 2)
     if (triangles && triangles.length != 0) {
-        console.log('triangles', triangles)
         for (let i = 0; i < triangles.length; i++) {
-            let tlength = triangles.length
-            if (i % 3 == 0 && i < tlength - 2) {
-                faceVertices.push(triangles[i], triangles[i + 1], triangles[i + 2]) // 底部的三角面
-                faceVertices.push(triangles[i] + length, triangles[i + 1] + length, triangles[i + 2] + length) // 顶部的三角面
+            if (i % 3 == 0) {
+                let xIndex = triangles[i]
+                let yIndex = triangles[i + 1]
+                let zIndex = triangles[i + 2]
+                faceVertices.push(points[xIndex], points[yIndex], points[zIndex]) // 底部的三角面
+                faceVertices.push(topPoints[xIndex], topPoints[yIndex], topPoints[zIndex]) // 顶部的三角面
             }
         }
     }
+
     const geometry = new THREE.BufferGeometry()
     const faceVerticesFloatArray = new Float32Array(faceVertices.flat())
     geometry.setAttribute('position', new THREE.BufferAttribute(faceVerticesFloatArray, 3))
